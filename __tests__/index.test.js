7c1878bccf57ca3521c528d6135bf0f0f4ba9ca7
hao <haleluohao@gmail.com>
2019-09-10 22:03:40 +0100

    rewrite it to load everything in one go

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -76,27 +76,28 @@ describe("MardownHandler Test: index.js", () => {
 
     describe("markdownHandler.loadMds()", () => {
 
-        const md = `
-        ---
-        title:This is a test, this is the first title
-        thumbnail:abc.jpg
-        tags:a,b,c,d
-        categories:fun,food
-        date:
-        --- 
-        Some random words in the middle.
-        ![sporule](https://testa.com/abc.jpg)
-        ![sporule](https://test.com)
-        ### This is a test, this is the second title
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et 
-        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
-        ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-        `;
-        fetch.mockResponse(md);
+
         const paths = ["https://www.sporule.com/hello_1987-12-05_happy,fun@abc.md", "https://www.sporule.com/hello_1987-12-05_happy,fun@abc.md"];
         describe("All 'MUST HAVE' metas must be presented, otherwise it will not get return in items", () => {
+            const md = `
+            ---
+            title:This is a test, this is the first title
+            thumbnail:abc.jpg
+            tags:a,b,c,d
+            categories:fun,food
+            date:
+            --- 
+            Some random words in the middle.
+            ![sporule](https://testa.com/abc.jpg)
+            ![sporule](https://test.com)
+            ### This is a test, this is the second title
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et 
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+            `;
+            fetch.mockResponse(md);
             test("The items length should be 2 as it has all the necessary metas", () => {
                 let markdownHandler = new MarkdownHandler();
                 return markdownHandler.loadMds(paths).then(result => {
@@ -117,12 +118,86 @@ describe("MardownHandler Test: index.js", () => {
         });
 
         test("It should parse the meta data correctly such as title", () => {
+            const md = `
+            ---
+            title:This is a test, this is the first title
+            thumbnail:abc.jpg
+            tags:a,b,c,d
+            categories:fun,food
+            date:
+            --- 
+            Some random words in the middle.
+            ![sporule](https://testa.com/abc.jpg)
+            ![sporule](https://test.com)
+            ### This is a test, this is the second title
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et 
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+            `;
+            fetch.mockResponse(md);
             let markdownHandler = new MarkdownHandler();
             return markdownHandler.loadMds(paths).then(result => {
                 const actual = result.items[0].metas["title"];
                 const expected = "this is a test, this is the first title";
                 expect(actual).toEqual(expected);
             });
+        });
+
+        describe("Thumbnail should be extracted from the markdown or used default thumbnail", () => {
+            test("Thumbnail should be extracted from the markdown file as it is in the markdown file ", () => {
+                const md = `
+                ---
+                title:This is a test, this is the first title
+                tags:a,b,c,d
+                categories:fun,food
+                date:
+                --- 
+                Some random words in the middle.
+                ![sporule](https://testa.com/abc.jpg)
+                ![sporule](https://test.com)
+                ### This is a test, this is the second title
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et 
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum.
+                `;
+                fetch.mockResponse(md);
+                let markdownHandler = new MarkdownHandler();
+                return markdownHandler.loadMds(paths).then(result => {
+                    const actual = result.items[0].metas["thumbnail"];
+                    const expected = "https://testa.com/abc.jpg";
+                    expect(actual).toEqual(expected);
+                });
+            });
+
+            test("Thumbnail should be the default thumbnail as markdown file does not contain any image ", () => {
+                const md = `
+                ---
+                title:This is a test, this is the first title
+                tags:a,b,c,d
+                categories:fun,food
+                date:
+                --- 
+                Some random words in the middle.
+                ### This is a test, this is the second title
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et 
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum.
+                `;
+                fetch.mockResponse(md);
+                let markdownHandler = new MarkdownHandler();
+                return markdownHandler.loadMds(paths).then(result => {
+                    const actual = result.items[0].metas["thumbnail"];
+                    const expected = markdownHandler.defaultThumbnail;
+                    expect(actual).toEqual(expected);
+                });
+            });
+
         })
     });
 });
